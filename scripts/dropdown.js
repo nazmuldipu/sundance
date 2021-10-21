0dcaaaf0fea2e406c758efa47c3dda3a90d0c266
sarjis-abdullah <sarjis@reformedtech.org>
2021-10-21 18:44:36 +0600

    js functionality improved

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,25 +1,43 @@
-const dropdownBox = document.querySelector(".dropdown--box");
-const { classList } = document.querySelector(".dropdown--options");
-const optionsList = document.querySelectorAll(".dropdown--option");
-const selected = document.querySelector(".dropdown__selectable-item");
-const dropdownIcon = document.getElementById("dropdown--icon");
-const dropdownText = document.getElementById("dropdown--text");
-selected.addEventListener("click", () => {
-  classList.toggle("active");
-  if (!dropdownBox.style.zIndex || dropdownBox.style.zIndex == 0) {
-    dropdownBox.style.zIndex = 4;
-  } else {
-    dropdownBox.style.zIndex = 0;
-  }
-});
-selected.addEventListener("blur", () => {
-  classList.remove("active");
-  dropdownBox.style.zIndex = 0;
-});
+let dropdownBox = null;
+let dropdownOptions = null;
+let optionsList = null;
+let selected = null;
+let dropdownIcon = null;
+let dropdownText = null;
+let bool = false;
+
+const onclickDropdown = (id)=> {
+    bool = true;
+    dropdownBox = document.querySelector(`.dropdown--box__${id}`);
+    dropdownBox.classList.toggle("active");
+    dropdownOptions = document.querySelector(`.${id}--options`);
+    optionsList = document.querySelectorAll(`.dropdown--option__${id}`);
+    dropdownOptions.classList.toggle("active");
+    dropdownIcon = document.querySelector(`.${id}__icon`);
+    dropdownText = document.querySelector(`.${id}__text`);
+    selected = document.querySelector(".item__" + id);
+    
+    if (dropdownBox.classList.contains("active")) {
+      dropdownBox.style.zIndex = 4;
+    } else {
+      dropdownBox.style.zIndex = 0;
+    }
+}
+
+const onblurDropdown = ()=> {
+    if (bool) {
+        dropdownOptions.classList.remove("active");
+        dropdownBox.style.zIndex = 0;
+    }
+}
+
+if(bool){
 optionsList.forEach((o) => {
   o.addEventListener("click", () => {
     dropdownText.innerHTML = o.querySelector("label").innerHTML;
     selected.innerHTML = dropdownText.outerHTML + dropdownIcon.outerHTML;
-    classList.remove("active");
+    dropdownOptions.classList.remove("active");
   });
 });
+}
+export {onclickDropdown, onblurDropdown}
