cdad7f71fa6e1c017e476b6be279f820254c472d
akhost <akhost@gmail.com>
2021-10-26 10:48:02 -0400

    dev deployment code

--- deploy_hotel_site.sh
+++ deploy_hotel_site.sh
@@ -11,6 +11,7 @@ npm run build
 
 #check to make sure that this bash supports substring expansion
 echo $BASH_VERSION
+echo $S3_BUCKET_NAME
 
 cd build
 
@@ -19,19 +20,7 @@ shopt -s nullglob
 html_files=(*.html)
 
 #sync from build directory to s3 bucket
-aws s3 sync ./ s3://$S3_BUCKET_NAME --acl public-read --exclude "*.html" #--dryrun
-
-#copy each .html file and rename it without file extension on s3
-for i in "${html_files[@]}"
-do
-    object_name=${i:0:-5}
-    echo "${object_name}"
-    aws s3 cp ${i} s3://$S3_BUCKET_NAME/${object_name} --acl public-read --content-type "text/html" #--dryrun
-done
-
-#rm synced .html files
-aws s3 rm s3://$S3_BUCKET_NAME --recursive --exclude "*" --include "*.html" #--dryrun
-
+aws s3 sync ./ s3://$S3_BUCKET_NAME --acl public-read 
 
 #create a new Cloudfront invalidation as soon as the built files have been uploaded to S3
 aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DIST_ID --paths "/*"
@@ -40,4 +29,4 @@ aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DIST_ID --paths
 # echo "It's not a stage branch"
 # exit 1
 
-fi
+#fi
