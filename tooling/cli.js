ee5dcfdabcfc7a06effcfe82d4752e516c29b0f0
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2021-11-12 11:29:11 +0600

    fixed cli path issue

--- tooling/cli.js
+++ tooling/cli.js
@@ -1,23 +1,32 @@
 import fs from 'fs';
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
+import { PAGES_DIR } from './lib.js';
 
 
-const template = `
----
-layout: base.njk
-pagename: sample
-permalink: "build/sample/index.html"
-permalinkBypassOutputDir: true
-nickname: "Sample"
----
-<div class="sample"> sample page </div>`;
+const templates = {
+  template: `
+  ---
+  layout: base.njk
+  pagename: sample
+  permalink: "build/sample/index.html"
+  permalinkBypassOutputDir: true
+  nickname: "Sample"
+  ---
+  <div class="sample"> sample page </div>`,
+  css: '',
+  json: '',
+}
+
 
 const fileExists = path => file => fs.existsSync(`${path}/${file}`);
 
 const writeToPath = path => (file, content) => {
   const filePath = `${path}/${file}`;
-
+  if (!fs.existsSync(path)){
+    fs.mkdirSync(path);
+  }
+  console.log(content);
   fs.writeFile(filePath, content, err => {
     if (err) throw err;
     console.log("Created file: ", filePath);
@@ -27,7 +36,10 @@ const writeToPath = path => (file, content) => {
 
 function createFiles(path, name) {
   const files = {
-    index: "index.jsx",
+    template: `${name}.njk`,
+    css: `${name}-sync.css`,
+    js: `${name}-module.js`,
+    json: `${name}.json`,
   };
   const writeFile = writeToPath(path);
   const toFileMissingBool = file => !fileExists(path)(file);
@@ -40,14 +52,17 @@ function createFiles(path, name) {
   if (noneExist) {
     console.log(`Detected new file: ${name}, ${path}`);
     Object.entries(files).forEach(([type, fileName]) => {
-      writeFile(fileName, templates[type](name));
+      writeFile(fileName, templates[type]);
     });
   }
 }
+
 /* yargs will be used for better command line interface
 const parsedArguments = yargs(hideBin(process.argv)).argv;
 console.log(parsedArguments);
 */
 const fileName = process.argv[2];
-console.log(`Creating new file: ${fileName}`);
+if(fileName){
+  createFiles(PAGES_DIR.pathname + `/${fileName}`, fileName);
+}
 
