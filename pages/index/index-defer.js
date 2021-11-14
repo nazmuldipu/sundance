fc5bf175ab03cf3d9f01defe3cd3d4c238307143
Nazmul Alam <nazmuldipu@gmail.com>
2021-11-14 21:30:45 +0600

    hero image added to the slider

--- pages/index/index-defer.js
+++ pages/index/index-defer.js
@@ -1,3 +1,17 @@
+// hero-carousel
+const heroCarousel = document.querySelectorAll(".hero-carousel__frame");
+heroCarousel.forEach(function (slide_frame) {
+    slide_frame.addEventListener("click", function (frame) {
+        if (!frame.target.classList.contains("active")) {
+            const parent_ele = frame.target.parentElement;
+            const active_elements = parent_ele.querySelectorAll(".active");
+            active_elements.forEach(function (active_element) {
+                active_element.classList.remove("active");
+                frame.target.classList.add("active");
+            });
+        }
+    });
+});
 // thumbnail slider events
 const slide_frame = document.getElementById("thumbnail-slider__image__frame");
 const add_image_click_event = () => {
