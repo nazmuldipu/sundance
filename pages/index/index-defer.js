08d974cbbc3d9ff98b63d5de330da8e55579e76b
Nazmul Alam <nazmuldipu@gmail.com>
2021-11-15 19:07:32 +0600

    home page design updated

--- pages/index/index-defer.js
+++ pages/index/index-defer.js
@@ -1,25 +1,27 @@
 // hero-carousel
-const heroCarousel = document.querySelectorAll(".hero-carousel__frame");
-heroCarousel.forEach(function (slide_frame) {
+const hero_image_frames = document.querySelectorAll(".hero-carousel__frame .image_frame");
+hero_image_frames.forEach(function (slide_frame) {
     slide_frame.addEventListener("click", function (frame) {
-        if (!frame.target.classList.contains("active")) {
-            const parent_ele = frame.target.parentElement;
-            const active_elements = parent_ele.querySelectorAll(".active");
+        const parent_ele = frame.target.parentElement.parentElement;
+        if(!parent_ele.classList.contains("active")){
+            const active_elements = parent_ele.parentElement.querySelectorAll(".active");
             active_elements.forEach(function (active_element) {
                 active_element.classList.remove("active");
-                frame.target.classList.add("active");
+                parent_ele.classList.add("active");
             });
         }
     });
 });
+
 // thumbnail slider events
-const slide_frame = document.getElementById("thumbnail-slider__image__frame");
 const add_image_click_event = () => {
     const slider_images = document.querySelectorAll(
         ".thumbnail-slider__slides__box .image_frame"
     );
     slider_images.forEach(function (slide) {
         slide.addEventListener("click", function () {
+            const frame_id = slide.parentElement.dataset.framename;
+            const slide_frame = document.getElementById(frame_id);
             slide_frame.firstElementChild.innerHTML = slide.innerHTML;
         });
     });
