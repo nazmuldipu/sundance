f0049f8aaa8732c67fdc9dea6508ffa8cc93b6ab
meherab-hossain <h.meherabhossain@gmail.com>
2021-11-17 12:07:01 +0600

    import statement added

--- pages/activities/summer-activities/scenic-lift-rides/scenic-lift-rides-module.js
+++ pages/activities/summer-activities/scenic-lift-rides/scenic-lift-rides-module.js
@@ -1,7 +1,3 @@
-import { lazyLoadVideos } from '../../../../scripts/lazyloading.js';
-import '../../../../components/index.js';
-import "../../../../components/MediaCarousel/index.js";
+import "components/MediaCarousel/index.js";
 
-document.addEventListener("DOMContentLoaded", lazyLoadVideos('lazy', { 
-    rootMargin: '100px' // will start loading video with 100px buffer
- }));
+
