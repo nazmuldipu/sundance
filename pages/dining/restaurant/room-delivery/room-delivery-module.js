0ef236443519aea43ffd8d61967744ec79e2eca1
sarjis-abdullah <sarjis@reformedtech.org>
2021-11-17 11:44:01 +0600

    prev issue solved

--- pages/dining/restaurant/room-delivery/room-delivery-module.js
+++ pages/dining/restaurant/room-delivery/room-delivery-module.js
@@ -1,7 +1,1 @@
-import { lazyLoadVideos } from '../../../../scripts/lazyloading.js';
-import '../../../../components/index.js';
-import "../../../../components/MediaCarousel/index.js";
-
-document.addEventListener("DOMContentLoaded", lazyLoadVideos('lazy', { 
-    rootMargin: '100px' // will start loading video with 100px buffer
- }));
+import "components/MediaCarousel/index.js";
