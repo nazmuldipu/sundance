63f604c9c29051a5cf57cd19665da2ea73e9405e
sarjis-abdullah <sarjis@reformedtech.org>
2021-11-17 12:40:13 +0600

    foundry grill done

--- pages/dining/restaurant/foundry-grill/foundry-grill.11tydata.cjs
+++ pages/dining/restaurant/foundry-grill/foundry-grill.11tydata.cjs
@@ -1,3 +1,3 @@
 const getAllData = require('../../../../scripts/utils/get-page-data.cjs')
 
-module.exports  = getAllData('/dining-restaurant-details')
+module.exports  = getAllData('/dining/restaurant/foundry-grill')
