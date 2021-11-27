be5275af3d5a61c26f83ede68a9e23fda01a5e93
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2021-11-27 18:55:11 +0600

    refactor

--- components/MediaCarouselWithThumbnail/index.js
+++ components/MediaCarouselWithThumbnail/index.js
@@ -1,49 +1,30 @@
 "use strict";
-import ProgressiveElement from "../progressive-element.js";
+import MediaCarousel from "../MediaCarousel/index.js";
 
-
-export default class MediaCarouselWithThumbnail extends ProgressiveElement {
-  constructor() {
-    super([
-      {
-        id: "swiper",
-        behaviorPath: "/../lib/swiper.js", // have to use relative path
-        stylePath: "https://unpkg.com/swiper/swiper-bundle.min.css",
-        type: "IntersectionObserver",
-        observerConfig: {
-          rootMargin: "400px",
-        },
-      },
-    ]);
-  }
-
-  // assuming use of IntersectionObserver
-  _onLoad(moduleId, entries, observer) {
-    if (entries.some((entry) => entry.isIntersecting)) {
-      super._onLoad(moduleId).then(( res ) => {
-        const mod = res[1];
-        if (mod.Swiper) {
-          this.thumbnailInstance = new mod.Swiper(
-            this.getElementsByClassName("swiper-thumbnail-container")?.[0],
-            mod.defaultConfig
-          );
-          this.swiperInstance = new mod.Swiper(
-            this.getElementsByClassName("swiper-container")?.[0],
-            Object.assign(mod.defaultConfig, { thumbs: { swiper: this.thumbnailInstance } })
-          );
-          this.swiperInstance.on("realIndexChange", function (e) {
-            this.el.dispatchEvent(
-              new CustomEvent("slideChanged", {
+export default class MediaCarouselWithThumbnail extends MediaCarousel {
+  
+  setupSwiper(mod){
+    console.log(mod);  
+    this.thumbnailInstance = new mod.Swiper(
+        this.getElementsByClassName("swiper-thumbnail-container")?.[0],
+        mod.defaultConfig
+    );
+    this.swiperInstance = new mod.Swiper(
+        this.getElementsByClassName("swiper-container")?.[0],
+        Object.assign(mod.defaultConfig, { thumbs: { swiper: this.thumbnailInstance } })
+    );
+    console.log(this.swiperInstance);
+    this.swiperInstance.on("realIndexChange", function (e) {
+        this.el.dispatchEvent(
+            new CustomEvent("slideChanged", {
                 detail: {
-                  realIndex: e.realIndex,
+                    realIndex: e.realIndex,
                 },
-              })
-            );
-          });
-        }
-      });
-    }
+            })
+        );
+    });  
   }
+
 }
 
 customElements.define("media-carousel-thumbnail", MediaCarouselWithThumbnail);
