bab1f023ba83353efc8024891f19c786f6d136ec
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2021-11-29 01:27:31 +0600

    updated config

--- components/MediaCarouselWithThumbnail/index.js
+++ components/MediaCarouselWithThumbnail/index.js
@@ -6,16 +6,17 @@ export default class MediaCarouselWithThumbnail extends MediaCarousel {
   setupSwiper(mod){ 
     const config = {
         loop: true,
-        spaceBetween: 5
+        loopedSlides: 7
     }  
     this.thumbnailInstance = new mod.Swiper(
         this.getElementsByClassName("swiper-thumbnail-container")?.[0],
-        Object.assign({}, mod.defaultConfig, {observeParents: true, noSwiping : true, ...config})
+        Object.assign({}, mod.defaultConfig, {observeParents: true, ...config})
     );
     this.swiperInstance = new mod.Swiper(
         this.getElementsByClassName("swiper-container")?.[0],
-        Object.assign(mod.defaultConfig, { thumbs: { swiper: this.thumbnailInstance, observeParents: true, ...config} })
+        Object.assign(mod.defaultConfig, { thumbs: { swiper: this.thumbnailInstance, observeParents: true, noSwiping : true, ...config} })
     );
+
     this.swiperInstance.on("realIndexChange", function (e) {
         this.el.dispatchEvent(
             new CustomEvent("slideChanged", {
