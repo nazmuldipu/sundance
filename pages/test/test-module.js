c307443d9691b76879b9d6237e32d841e253f859
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2021-11-29 13:08:26 +0600

    fixed loadCSS error

--- pages/test/test-module.js
+++ pages/test/test-module.js
@@ -2,7 +2,7 @@
 import { Swiper, defaultConfig } from 'components/lib/swiper.js';
 import ProgressiveElement from 'components/progressive-element.js';
 document.addEventListener('DOMContentLoaded', () => {
-    ProgressiveElement.loadCss('https://unpkg.com/swiper/swiper-bundle.min.css').then(() => {
+    ProgressiveElement.loadCSS('https://unpkg.com/swiper/swiper-bundle.min.css').then(() => {
         var swiper = new Swiper(".mySwiper", {    loop: true,    spaceBetween: 10,    slidesPerView: 4,    freeMode: true,    watchSlidesProgress: true  });
         var swiper2 = new Swiper(".mySwiper2", {    loop: true,    spaceBetween: 10,    navigation: {      nextEl: ".swiper-button-next",      prevEl: ".swiper-button-prev"    },    thumbs: {      swiper: swiper    }});
     })
