123e36c4b5eccf2eff33588c89e017508da2fe46
Nazmul Alam <nazmuldipu@gmail.com>
2021-12-06 14:31:02 +0600

    carousel title clicke event added

--- pages/index/index-defer.js
+++ pages/index/index-defer.js
@@ -2,16 +2,29 @@
 
 // hero-carousel
 const hero_image_frames = document.querySelectorAll(".hero-carousel__frame .image_frame");
+const hero_carousel__title = document.querySelectorAll(".hero-carousel__title");
+
+const update_active_element = (element) => {
+    if(!element.classList.contains("active")){
+        const active_elements = element.parentElement.querySelectorAll(".active");
+        active_elements.forEach(function (active_element) {
+            active_element.classList.remove("active");
+            element.classList.add("active");
+        });
+    }
+}
+
 hero_image_frames.forEach(function (slide_frame) {
     slide_frame.addEventListener("click", function (frame) {
         const parent_ele = frame.target.parentElement.parentElement;
-        if(!parent_ele.classList.contains("active")){
-            const active_elements = parent_ele.parentElement.querySelectorAll(".active");
-            active_elements.forEach(function (active_element) {
-                active_element.classList.remove("active");
-                parent_ele.classList.add("active");
-            });
-        }
+        update_active_element(parent_ele);
+    });
+});
+
+hero_carousel__title.forEach(function (title) {
+    title.addEventListener("click", function () {
+        const parent_ele = title.parentElement.parentElement;
+        update_active_element(parent_ele);
     });
 });
 
@@ -23,17 +36,4 @@ const add_image_click_event = () => {
     slider_images.forEach(function (slide) {
         slide.addEventListener("click", function () {
             const frame_id = slide.parentElement.dataset.framename;
-            const slide_frame = document.getElementById(frame_id);
-            slide_frame.firstElementChild.innerHTML = slide.innerHTML;
-        });
-    });
-};
-
-const carousel_buttons = document.querySelectorAll(".carousel-button");
-carousel_buttons.forEach(function (button) {
-    button.addEventListener("click", function () {
-        add_image_click_event();
-    });
-});
-
-add_image_click_event();
+            const slide_frame = document.getElem
