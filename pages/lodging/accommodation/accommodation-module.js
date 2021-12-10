4292b108e99f70a861ab49ff20f3927faa99d786
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2021-12-11 01:45:47 +0600

    loaded data locally, added event listener for all checkboxes

--- pages/lodging/accommodation/accommodation-module.js
+++ pages/lodging/accommodation/accommodation-module.js
@@ -1,1 +1,10 @@
+import '../../../scripts/lib/globalEvents.js';
 import "components/MediaCarousel/index.js";
+
+const filter_container = document.querySelector('.filter__container');
+
+console.log(filter_container);
+
+filter_container.addEventListener('click', (e) => {
+    console.log(e.target);
+})
--- pages/lodging/accommodation/accommodation.11tydata.cjs
+++ pages/lodging/accommodation/accommodation.11tydata.cjs
@@ -1,3 +1,17 @@
-const getAllData = require('../../../scripts/utils/get-page-data.cjs')
+const fs = require('fs');
+const getAllData = require('../../../scripts/utils/get-page-data.cjs');
 
-module.exports  = getAllData('/lodging/accommodation')
+module.exports  = (async function(){
+    const data = await getAllData('/lodging/accommodation');
+    const additionalData = require('./accommodation-data.json');
+    if(data){
+        fs.writeFileSync('./pages/lodging/accommodation/accommodation.11tydata.json', JSON.stringify({
+            ...data,
+            ...additionalData
+        },  null, '\t'));
+    }
+    return {
+        ...data,
+        ...additionalData
+    }
+})();
