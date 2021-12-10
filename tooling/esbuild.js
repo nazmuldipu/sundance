8cc0d05e1ab42a1345627648a7bb1b59bce5f677
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2021-12-10 09:28:38 +0600

    remove console.log

--- tooling/esbuild.js
+++ tooling/esbuild.js
@@ -85,7 +85,7 @@ export const addGlobalBehavior = async (pagesDir, scriptsDir) => {
         }
         return promises;
     }, []);
-
+    
     await Promise.all(promises);
 
     return fileBytePairs;
