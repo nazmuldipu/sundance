ae21f4b518b93cc64c01cb9e66eacd928678d0bd
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2021-12-12 00:04:02 +0600

    filter feature added :-)

--- pages/lodging/accommodation/accommodation-module.js
+++ pages/lodging/accommodation/accommodation-module.js
@@ -2,7 +2,7 @@ import '../../../scripts/lib/globalEvents.js';
 import "components/MediaCarousel/index.js";
 
 const filter_container = document.querySelector('.filter__container');
-const cards = document.querySelectorAll('.flex__single-card')
+const cards = Array.from(document.querySelectorAll('.flex__single-card'));
 const cardsContainer = document.querySelector('.flex-card-container');
 
 const filters = []
@@ -29,29 +29,25 @@ filter_container.addEventListener('click', (e) => {
     }
 })
 
+const getValue = object => object.value;
+
 /**
  * 
  * @param {array} filters 
  */
 function applyFilters(filters){
-    console.log(filters);
-    const getValue = object => object.value;
     const appliedBedrooms = filters.filter(filter => filter.type === 'bedrooms').map(getValue);
     const appliedSleeps = filters.filter(filter => filter.type === 'sleeps').map(getValue);
     const appliedType = filters.filter(filter => filter.type === 'type').map(getValue); 
-    console.log({appliedBedrooms, appliedSleeps, appliedType})
-    cards.forEach(card => {
+    cards.forEach(card => card.classList.remove('hidden'));
+    const filteredCards = cards.filter( card => {
         const cardObject = card.dataset.card ? JSON.parse(card.dataset.card) : {};
         const { Type: type, Sleeps: sleeps, Bedrooms: bedrooms } = cardObject;
-        console.log({type, sleeps, bedrooms})
-        if(appliedBedrooms.length > 0 && bedrooms && !appliedBedrooms.some(item => bedrooms.includes(item))){
-            card.style.display = 'none';
-        }else if(appliedSleeps.length > 0 && sleeps && !appliedSleeps.some(item => sleeps.includes(item))){
-            card.style.display = 'none';
-        }else if(appliedType.length > 0 && type && !appliedType.some(item => type.includes(item))){
-            card.style.display = 'none';
-        }
-
-    })
+        const isBedroomsFilterAdded = appliedBedrooms.length > 0 && bedrooms && !appliedBedrooms.some(item => bedrooms.includes(item));
+        const isSleepsFilterAdded = appliedSleeps.length > 0 && sleeps && !appliedSleeps.some(item => sleeps.includes(item));
+        const isTypeFilterAdded = appliedType.length > 0 && type && !appliedType.some(item => type.includes(item));
+        return isBedroomsFilterAdded || isSleepsFilterAdded || isTypeFilterAdded;      
+    });
+    filteredCards.forEach(card => card.classList.add('hidden'));
 }
 
