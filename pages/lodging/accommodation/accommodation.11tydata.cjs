eb17b767fd9df69ac361d322f367aa480515053a
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2021-12-13 15:56:30 +0600

    generated filter from data

--- pages/lodging/accommodation/accommodation.11tydata.cjs
+++ pages/lodging/accommodation/accommodation.11tydata.cjs
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const getAllData = require('../../../scripts/utils/get-page-data.cjs');
-
+const saveData = false;
 module.exports  = (async function(){
     const data = await getAllData('/lodging/accommodation');
     const additionalData = require('./accommodation-data.json');
-    if(data){
+    if(data && additionalData && saveData){
         fs.writeFileSync('./pages/lodging/accommodation/accommodation.11tydata.json', JSON.stringify({
             ...data,
             ...additionalData
