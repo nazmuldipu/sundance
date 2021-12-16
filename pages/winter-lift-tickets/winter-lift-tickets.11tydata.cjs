51c575ab9aff84c61565b521be483347b1098c32
sarjis-abdullah <sarjis@reformedtech.org>
2021-12-16 18:37:04 +0600

    winter lift cards created

--- pages/winter-lift-tickets/winter-lift-tickets.11tydata.cjs
+++ pages/winter-lift-tickets/winter-lift-tickets.11tydata.cjs
@@ -1,2 +1,3 @@
-const getAllData = require('../../scripts/utils/get-page-data.cjs');
-module.exports  = getAllData('/winter-lift-tickets');
+const getAllData = require('../../scripts/utils/get-page-data.cjs')
+
+module.exports  = getAllData('/winter-lift-tickets')
