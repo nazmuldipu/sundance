46beecf4243cd7aa0555f3099819d32c3ba712ea
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2021-12-23 16:21:49 +0600

    added video feed to popup

--- components/custom-elements/video-cam-component.js
+++ components/custom-elements/video-cam-component.js
@@ -2,28 +2,21 @@ import ProgressiveElement from "../progressive-element.js";
 
 export default class VideoCam extends ProgressiveElement{
     constructor() {
-        super([
-          {
-            id: "violet",
-            behaviorPath: "//js.prismcam.com/violet.js", // have to use relative path
-            stylePath: "//js.prismcam.com/violet.css",
-            type: "IntersectionObserver",
-            observerConfig: {
-              rootMargin: "400px",
-            },
-          },
-        ]);
-      }
-
-        // assuming use of IntersectionObserver
-  _onLoad(moduleId, entries, observer) {
-    if (entries.some((entry) => entry.isIntersecting)) {
-      super._onLoad(moduleId).then(( res ) => {
-        const mod = res[1];
-        console.log(mod);
-      });
+        super([]);
     }
-  }
+
+    connectedCallback(){
+        const el = this.querySelector('#feed-container');
+/*         window.addEventListener('video-loaded', ()=> {
+            el.appendChild(window.video);
+        }) */
+        setTimeout(() => {
+            if(window.video){
+                el.appendChild(window.video);
+            }
+        }, 3000)
+    }
+  
 }
 
 customElements.define("video-cam", VideoCam);
