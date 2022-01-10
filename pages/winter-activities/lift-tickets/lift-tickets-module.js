f5bb326c92ef8474464848dc733c8711decea80e
meherab-hossain <h.meherabhossain@gmail.com>
2022-01-10 18:12:17 +0600

    added listener

--- pages/winter-activities/lift-tickets/lift-tickets-module.js
+++ pages/winter-activities/lift-tickets/lift-tickets-module.js
@@ -1,1 +1,23 @@
 import '../../../scripts/lib/globalEvents.js';
+
+
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    let test= document.querySelector('media-carousel');
+    test.addEventListener('slideChanged',function (e) {
+        console.log(e);
+    })
+});
+
+// Array.from(test).forEach(function(element) {
+//     console.log(element[0])
+// });
+// const mySwiper = new Swiper('.swiper-container', {
+//     on : {
+//         slideChange: function (e) {
+//             console.log(e);
+//          }
+//         }
+//   });
+  
