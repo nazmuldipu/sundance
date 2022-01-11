76c4a40018330678253091298631de312535b472
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2022-01-12 01:00:00 +0600

    updated page data urls

--- pages/dining/tree-room/tree-room.11tydata.cjs
+++ pages/dining/tree-room/tree-room.11tydata.cjs
@@ -1,3 +1,3 @@
 const getAllData = require('../../../scripts/utils/get-page-data.cjs')
 
-module.exports  = getAllData('/dining/restaurants/tree-room')
+module.exports  = getAllData('/dining/tree-room')
