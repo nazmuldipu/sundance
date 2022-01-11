8bd83e6d5cbd0313c9f84e92e6339cfb3732544a
Nazmul Alam <nazmuldipu@gmail.com>
2022-01-11 11:07:05 +0600

    toogle comp created

--- pages/wedding-rfp/wedding-rfp-defer.js
+++ pages/wedding-rfp/wedding-rfp-defer.js
@@ -1,8 +1,15 @@
 console.log('wedding-rfp-defer.js')
+function toggleDiv(obj, title){
+   if(obj && obj.id){
+       console.log(title);
+       console.log(obj.value, obj)
+       console.log(Boolean(obj.value));
+   }
+}
+toggleDiv();
+// const childElements = document.querySelectorAll('.has-child');
 
-const childElements = document.querySelectorAll('.has-child');
-
-console.log(childElements);
-childElements.forEach(function(element) {
-    console.log(element.dataset.child);
-})
+// console.log(childElements);
+// childElements.forEach(function(element) {
+//     console.log(element.dataset.child);
+// })
