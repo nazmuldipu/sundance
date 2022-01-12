770b3dfe9d77c59f0c424b0fda498cf5d96f70ab
ashfaq-skipper <ashfaqur@skipperhospitality.com>
2022-01-12 10:02:50 +0600

    Page url updated

--- pages/sundance-mountain-outfitters/sundance-mountain-outfitters.11tydata.cjs
+++ pages/sundance-mountain-outfitters/sundance-mountain-outfitters.11tydata.cjs
@@ -1,3 +1,3 @@
 const getAllData = require('../../scripts/utils/get-page-data.cjs')
 
-module.exports  = getAllData('/sundance-mountain-outfitters')
+module.exports  = getAllData('/shop/sundance-mountain-outfitters')
