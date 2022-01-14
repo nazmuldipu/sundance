caaecffbb17fd414b023cb4b06a27c3d1d1ee01a
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2022-01-14 23:37:30 +0600

    not found will redirect to error page

--- pages/error/error-module.js
+++ pages/error/error-module.js
@@ -1,1 +1,6 @@
 import '../../scripts/lib/globalEvents.js';
+document.addEventListener('DOMContentLoaded', () => {
+    if(window.location.href !== window.location.origin + '/error/'){
+        window.location.href = window.location.origin + '/error/'
+    }
+})
