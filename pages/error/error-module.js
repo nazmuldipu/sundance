f70c98a77c1d23462ea4aa71db438fadbd13ff4e
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2022-01-15 00:01:29 +0600

    moved error handle code inline

--- pages/error/error-module.js
+++ pages/error/error-module.js
@@ -1,6 +1,1 @@
 /* ignore */
-document.addEventListener('DOMContentLoaded', () => {
-    if(window.location.href !== window.location.origin + '/error/'){
-        window.location.href = window.location.origin + '/error/'
-    }
-})
