63bb6407827fe7e5bf63dd241cedc76d651b474d
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2022-01-17 19:47:25 +0600

    Revert "fix #680 global header more button"

--- scripts/utils/nav-actions.js
+++ scripts/utils/nav-actions.js
@@ -1,11 +1,10 @@
 // Nav morebutton action
-// TODO for now, if needs then it can be used in future, Sarjis 17th Jan 2022
-// const nav_more_button = document.querySelector("#btn-nav-more");
-// const nav_more_menu = document.querySelectorAll(".nav-more-menus");
+const nav_more_button = document.querySelector("#btn-nav-more");
+const nav_more_menu = document.querySelectorAll(".nav-more-menus");
 
-// nav_more_button.addEventListener("mouseenter", function () {
-//     nav_more_menu.forEach(function (menu) {
-//         menu.classList.remove("hidden");
-//     })
-//     this.parentElement.classList.add("hidden");
-// });
+nav_more_button.addEventListener("mouseenter", function () {
+    nav_more_menu.forEach(function (menu) {
+        menu.classList.remove("hidden");
+    })
+    this.parentElement.classList.add("hidden");
+});
