dfb0936e046d80cc79070e7613601abed66cc187
sarjis-abdullah <sarjis@reformedtech.org>
2022-01-17 16:32:32 +0600

    made todo comment

--- scripts/utils/nav-actions.js
+++ scripts/utils/nav-actions.js
@@ -1,4 +1,5 @@
 // Nav morebutton action
+// TODO for now, if needs then it can be used in future, Sarjis 17th Jan 2022
 // const nav_more_button = document.querySelector("#btn-nav-more");
 // const nav_more_menu = document.querySelectorAll(".nav-more-menus");
 
